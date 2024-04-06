7865716e7abc1497c85c9b94939c20847a088a0d
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-06 19:02:46 +0530

    Update ServletInitializer.java

--- src/main/java/com/taskmanagement/ServletInitializer.java
+++ src/main/java/com/taskmanagement/ServletInitializer.java
@@ -1,33 +1,13 @@
  package com.taskmanagement;
 
-// import org.springframework.boot.builder.SpringApplicationBuilder;
-// import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-// public class ServletInitializer extends SpringBootServletInitializer {
-
-// 	@Override
-// 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-// 		return application.sources(TaskManagementApplication.class);
-// 	}
-
-// }
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.WebApplicationInitializer;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
+public class ServletInitializer extends SpringBootServletInitializer {
 
-public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(TaskManagementApplication.class);
-    }
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        super.onStartup(servletContext);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TaskManagementApplication.class);
+	}
 
 }
