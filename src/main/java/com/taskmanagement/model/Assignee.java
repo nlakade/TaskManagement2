d253eea5ca298ece23fea9602dfaba19bd7ed9e2
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-06 23:21:58 +0530

    Update Assignee.java

--- src/main/java/com/taskmanagement/model/Assignee.java
+++ src/main/java/com/taskmanagement/model/Assignee.java
@@ -4,9 +4,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "Assignee")
+
 public class Assignee {
 	
 	@Id
