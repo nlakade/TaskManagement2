ee5dfc5b9e60be5af5902ce61f7f7ce823460bc2
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-08 13:19:51 +0530

    Update Assignee.java

--- src/main/java/com/taskmanagement/model/Assignee.java
+++ src/main/java/com/taskmanagement/model/Assignee.java
@@ -6,8 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity 
-public class Assigner {
+@Entity
+public class Assignee {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -40,7 +40,7 @@ public class Assigner {
 	
 	@Column(name="Department")
 	private String department;
-
+	
 	public int getId() {
 		return id;
 	}
