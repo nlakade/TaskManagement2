520a9adc916ab8af3157081f1aef30e779642554
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-08 13:08:02 +0530

    Update AssigneeController.java

--- src/main/java/com/taskmanagement/controller/AssigneeController.java
+++ src/main/java/com/taskmanagement/controller/AssigneeController.java
@@ -2,8 +2,8 @@ package com.taskmanagement.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
