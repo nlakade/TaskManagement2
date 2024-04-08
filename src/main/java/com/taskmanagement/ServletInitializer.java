3399b949e9d37d7134c9dfb5f0ee12d7f018bc20
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-08 13:24:02 +0530

    Update ServletInitializer.java

--- src/main/java/com/taskmanagement/ServletInitializer.java
+++ src/main/java/com/taskmanagement/ServletInitializer.java
@@ -11,25 +11,18 @@
 // 	}
 
 // }
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
-public class ServletInitializer implements WebApplicationInitializer {
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        ctx.register(TaskManagementApplication.class);
-        ctx.setServletContext(servletContext);
+public class ServletInitializer extends SpringBootServletInitializer {
 
-        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TaskManagementApplication.class);
+	}
 
-        servlet.setLoadOnStartup(1);
-        servlet.addMapping("/");
-    }
+	
+
 }
