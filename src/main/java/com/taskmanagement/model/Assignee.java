f11502d6f98a2d24e5bc63b5f4959dc1d7679a29
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-09 15:52:48 +0530

    Update Assignee.java

--- src/main/java/com/taskmanagement/model/Assignee.java
+++ src/main/java/com/taskmanagement/model/Assignee.java
@@ -1,5 +1,5 @@
 package com.taskmanagement.model;
-
+import jakarta.persistence.*;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
