7c9e18916e134c15e20b09afa03ce4680c06e531
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-09 17:20:08 +0530

    Update ServletInitializer.java

--- src/main/java/com/taskmanagement/ServletInitializer.java
+++ src/main/java/com/taskmanagement/ServletInitializer.java
@@ -2,29 +2,12 @@ package com.taskmanagement;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
+public class ServletInitializer extends SpringBootServletInitializer {
 
-public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TaskManagementApplication.class);
+	}
 
-    @Override
-    protected Class<TaskManagementApplication> getApplicationClass() {
-        return TaskManagementApplication.class;
-    }
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.register(TaskManagementApplication.class);
-
-        servletContext.addListener(new ContextLoaderListener(context));
-
-        SpringApplication.run(getApplicationClass(), servletContext.getInitParameterNames(), servletContext.getInitParameterValues());
-    }
 }
