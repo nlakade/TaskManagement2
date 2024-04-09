d4ef27e7a8c3a377e647d64ff2403159cb6aa776
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-09 16:51:22 +0530

    Update ServletInitializer.java

--- src/main/java/com/taskmanagement/ServletInitializer.java
+++ src/main/java/com/taskmanagement/ServletInitializer.java
@@ -2,12 +2,12 @@ package com.taskmanagement;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(TaskManagementApplication.class);
     }
-
 }
