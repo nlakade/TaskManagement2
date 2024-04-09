b501ee0830a24f60c5fc252e6dc53c8a2df2a5bf
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-09 16:07:49 +0530

    Update AdminController.java

--- src/main/java/com/taskmanagement/controller/AdminController.java
+++ src/main/java/com/taskmanagement/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.taskmanagement.controller;
-import jakarta.persistence.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +9,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.taskmanagement.dao.AdminDao;
 import com.taskmanagement.model.Admin;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class AdminController {
