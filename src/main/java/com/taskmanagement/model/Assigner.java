22aaa6bde37003d6e9a9cace1a42810889e5dca3
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-09 19:54:04 +0530

    Update Assigner.java

--- src/main/java/com/taskmanagement/model/Assigner.java
+++ src/main/java/com/taskmanagement/model/Assigner.java
@@ -5,8 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity 
+@Table(name = "Assigner")
 public class Assigner {
 	
 	@Id
