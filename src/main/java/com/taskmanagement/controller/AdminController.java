4fa777306290f812962e6820db578cddcce4f6fb
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-09 15:51:12 +0530

    Update AdminController.java

--- src/main/java/com/taskmanagement/controller/AdminController.java
+++ src/main/java/com/taskmanagement/controller/AdminController.java
@@ -1,4 +1,5 @@
 package com.taskmanagement.controller;
+import jakarta.persistence.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
