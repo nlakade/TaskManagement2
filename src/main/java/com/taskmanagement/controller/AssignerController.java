867d36b79896b97e244ed99068d24088eb32715c
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-09 15:51:42 +0530

    Update AssignerController.java

--- src/main/java/com/taskmanagement/controller/AssignerController.java
+++ src/main/java/com/taskmanagement/controller/AssignerController.java
@@ -3,7 +3,7 @@ package com.taskmanagement.controller;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-
+import jakarta.persistence.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
