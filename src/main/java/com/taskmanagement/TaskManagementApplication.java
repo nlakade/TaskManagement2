aff7c2fe5146e646a05c976236c9108283263f8b
nlakade <153060160+nlakade@users.noreply.github.com>
2024-04-17 17:06:35 +0530

    Update TaskManagementApplication.java

--- src/main/java/com/taskmanagement/TaskManagementApplication.java
+++ src/main/java/com/taskmanagement/TaskManagementApplication.java
@@ -3,32 +3,12 @@ package com.taskmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Server9090 {
-
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Server9090.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "9090"));
-        app.run(args);
-    }
-}
 
 @SpringBootApplication
-public class Server9091 {
+public class TaskManagementApplication {
 
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Server9091.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "9091"));
-        app.run(args);
-    }
-}
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementApplication.class, args);
+	}
 
-@SpringBootApplication
-public class Server9092 {
-
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Server9092.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "9092"));
-        app.run(args);
-    }
 }
